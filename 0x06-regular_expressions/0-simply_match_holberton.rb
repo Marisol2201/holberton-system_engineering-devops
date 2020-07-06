20193adef1601ac321b73bc97405c5d668bf6b4b
Marisol2201 <1529@holbertonschool.com>
2020-07-06 23:03:14 +0000

    tasks0 to 7 v0

---
+++ 0x06-regular_expressions/0-simply_match_holberton.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+"""accepts one argument and pass it to a regular expression matching method"""
+"""The regular expression must match Holberton"""
+
+puts ARGV[0].scan(/Holberton/).join
