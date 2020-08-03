4bb108d94c7c5b6e16b8d67c9c905d88b5adeadb
Marisol2201 <1529@holbertonschool.com>
2020-08-03 21:52:41 +0000

    fixing possible problems

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
 # Using Puppet, create a file in /tmp
-
 file { '/tmp/holberton':
+    ensure  => file,
     path    => '/tmp/holberton',
-    mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
-    content => 'I love Puppet',
-    ensure  => 'file'
+    mode    => '0744',
+    content => 'I love Puppet'
 }
