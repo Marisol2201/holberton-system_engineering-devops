cf4e8f8588643ed8f86f7ea0d39ab492c660f04d
Marisol2201 <1529@holbertonschool.com>
2020-09-17 21:20:27 +0000

    task2 v0

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""queries the Reddit API and returns a list containing the titles of all hot
+articles for a given subreddit. If no results are found for the given
+subreddit, the function should return None"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """recursive function that queries the Reddit API"""
+    headers = {'User-agent': 'Marisol2201'}
+    base = 'https://www.reddit.com/'
+    query = '/r/{}/hot.json?after={}'.format(subreddit, after)
+    response = requests.get(base + query, headers=headers)
+    top = response.json().get('data', {}).get('children', [])
+    after = response.json().get('data', {}).get('after', None)
+    if not top:
+        return hot_list
+    for post in top:
+        hot_list.append(post.get('data').get('title'))
+    if not after:
+        return hot_list
+    return recurse(subreddit, hot_list, after)
