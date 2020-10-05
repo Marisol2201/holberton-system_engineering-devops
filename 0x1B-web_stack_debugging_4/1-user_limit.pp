e7d0b57a5f9ed300823ede88127acacd21f86436
Marisol2201 <1529@holbertonschool.com>
2020-10-05 18:04:34 +0000

    task1 v0

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,4 @@
+#Set broader user limits
+exec { 'change-os-configuration-for-holberton-user':
+  command => "/bin/sed -i -e 's/nofile 5/nofile 1000/g;s/nofile 4/nofile 1000/g' /etc/security/limits.conf"
+}
